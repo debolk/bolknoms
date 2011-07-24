ebd5e617b1c4746316bf772b046d709729caa8ef
Jakob Buis <jpabuis@cubiso.nl>
2011-07-24 21:40:50 +0200

    Improved table formatting by moving the delete icon in front of the attendees list

--- application/views/administratie/_meals.php
+++ application/views/administratie/_meals.php
@@ -2,21 +2,21 @@
     <table>
         <thead><tr>
             <th>Datum</th>
+            <th>&nbsp;</th>
             <th>Aantal</th>
             <th>Inschrijvingen</th>
-            <th>&nbsp;</th>
         </tr></thead>
         <tbody>
             <?php foreach ($meals as $meal): ?>
                 <tr>
                     <td class="date"><?php echo $meal; ?></td>
-                    <td class="number"><?php echo $meal->registrations->count_all(); ?></td>
-                    <td><?php echo $meal->registrations->as_list(); ?></td>
                     <td>
                         <a href="/administratie/verwijder/<?php echo $meal->id; ?>" class="confirmation-needed">
                             <img src="/images/cross.png" alt="Verwijderen"/>
                         </a>
                     </td>
+                    <td class="number"><?php echo $meal->registrations->count_all(); ?></td>
+                    <td><?php echo $meal->registrations->as_list(); ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
