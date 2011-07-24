ed1cfd18c986723d354fc6f1eb8ba8ffe6c1c6b1
Jakob Buis <jpabuis@cubiso.nl>
2011-07-24 21:37:40 +0200

    Added forgotten column rename [completed: 4]

--- migrations/201107242113_change_day_to_meal.sql
+++ migrations/201107242113_change_day_to_meal.sql
@@ -1,1 +1,9 @@
 RENAME TABLE `meals` TO `meals`;
+
+ALTER TABLE `registrations`
+	DROP FOREIGN KEY `registrations_day_id_days_id`;
+ALTER TABLE `registrations`
+	ALTER `day_id` DROP DEFAULT;
+ALTER TABLE `registrations`
+	CHANGE COLUMN `day_id` `meal_id` INT(10) UNSIGNED NOT NULL AFTER `name`,
+	ADD CONSTRAINT `registrations_meal_id_meals_id` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
