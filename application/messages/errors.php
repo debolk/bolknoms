cc958f131b9cc7991e311aa51af8e75d0e1cd212
Jakob Buis <jpabuis@cubiso.nl>
2011-07-24 21:28:09 +0200

    Errors now in Dutch [completed: 5]

--- application/messages/errors.php
+++ application/messages/errors.php
@@ -5,6 +5,7 @@
  */
 return array(
   'date' => array(
+                  'not_empty'                  => 'Je moet een datum invullen',
                   'Model_Meal::free_day'       => 'Op deze dag is al een maaltijd',
                   'Model_Meal::today_or_later' => 'Je kunt geen maaltijden in het verleden toevoegen',
                   'Model_Meal::valid_date'     => 'Je moet een geldige datum invoeren (yyyy-mm-dd)'
