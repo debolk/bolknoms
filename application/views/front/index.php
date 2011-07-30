cd33337625ef11329e54351e2928d8cbfb3b929f
Jakob Buis <jpabuis@cubiso.nl>
2011-07-30 18:39:15 +0200

    Instruction update for de-registering

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -56,7 +56,9 @@
 
 <h2>Afmelden</h2>
 <p>
-    Je kunt je op dit moment nog niet online afmelden. Neem contact op met het bestuur via
+    Na je aanmelding ontvang je een e-mail van bolknoms@bolk.nl ter bevestiging. Deze e-mail bevat een link om je uit te schrijven.
+    Let op: als de inschrijving is gesloten, kun je je ook niet meer afmelden voor een maaltijd.
+    Neem bij problemen contact op met het bestuur via
     <a href="mailto:bestuur@nieuwedelft.nl">bestuur@nieuwedelft.nl</a> of 015 212 60 12.
 </p>
 
