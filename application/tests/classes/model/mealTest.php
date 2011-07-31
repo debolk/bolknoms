b4a30823c2a3f2ec8c7b756e5e56219a315a921d
Jakob Buis <jpabuis@cubiso.nl>
2011-07-31 19:28:05 +0200

    Fixed a test

--- application/tests/classes/model/mealTest.php
+++ application/tests/classes/model/mealTest.php
@@ -9,17 +9,12 @@ class mealTest extends Kohana_UnitTest_TestCase
         // Clear database
         DB::query(Database::DELETE, 'DELETE FROM meals')->execute();
         DB::query(Database::DELETE, 'DELETE FROM registrations')->execute();
-
-        $yesterday = strftime('%Y-%m-%d', strtotime('yesterday'));
-        $today = strftime('%Y-%m-%d', strtotime('today'));
-        $tomorrow = strftime('%Y-%m-%d', strtotime('tomorrow'));
-        DB::query(Database::INSERT, "INSERT INTO meals (id, date) VALUES ('1', '$tomorrow'), ('2', '$yesterday'), ('3', '$today')")->execute();
     }
 
     public function testSavesAValidMeal()
     {
         $count = (int)ORM::factory('meal')->count_all();
-        ORM::factory('meal')->values(array('date' => '2020-01-01'))->save();
+        ORM::factory('meal')->values(array('date' => '2020-01-01', 'locked' => '14:00'))->save();
         $this->assertEquals($count+1, (int)ORM::factory('meal')->count_all());
     }
 
@@ -38,6 +33,11 @@ class mealTest extends Kohana_UnitTest_TestCase
 
     public function testHasDefaultOrdering()
     {
+        $yesterday = strftime('%Y-%m-%d', strtotime('yesterday'));
+        $today = strftime('%Y-%m-%d', strtotime('today'));
+        $tomorrow = strftime('%Y-%m-%d', strtotime('tomorrow'));
+        DB::query(Database::INSERT, "INSERT INTO meals (id, date) VALUES ('1', '$tomorrow'), ('2', '$yesterday'), ('3', '$today')")->execute();
+
         $meals = ORM::factory('meal')->find_all();
         $this->assertEquals(2,$meals->current()->id);
         $meals->next();
