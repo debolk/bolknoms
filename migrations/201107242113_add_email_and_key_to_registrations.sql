14c0326dd24ee52d4fd461079777dd5aefeece24
Jakob Buis <jpabuis@cubiso.nl>
2011-08-01 16:22:46 +0200

    Repaired migrations

--- migrations/201107242113_add_email_and_key_to_registrations.sql
+++ migrations/201107242113_add_email_and_key_to_registrations.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `registrations`
-	ADD COLUMN `email` VARCHAR(50) NOT NULL AFTER `meal_id`,
+	ADD COLUMN `email` VARCHAR(50) NOT NULL AFTER `day_id`,
 	ADD COLUMN `salt` VARCHAR(10) NOT NULL AFTER `email`
--- migrations/201107242113_change_day_to_meal.sql
+++ migrations/201107242113_change_day_to_meal.sql
@@ -1,9 +1,9 @@
-RENAME TABLE `days` TO `meals`;
-
 ALTER TABLE `registrations`
-	DROP FOREIGN KEY `registrations_day_id_days_id`;
+	DROP FOREIGN KEY `registrations_day_id_meals_id`;
+ALTER TABLE `days`
+	RENAME TO `meals`;
 ALTER TABLE `registrations`
 	ALTER `day_id` DROP DEFAULT;
 ALTER TABLE `registrations`
 	CHANGE COLUMN `day_id` `meal_id` INT(10) UNSIGNED NOT NULL AFTER `name`,
-	ADD CONSTRAINT `registrations_meal_id_meals_id` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+	ADD CONSTRAINT `registrations_meal_id__meals_id` FOREIGN KEY (`meal_id`) REFERENCES `meals` (`id`) ON UPDATE CASCADE ON DELETE CASCADE;
