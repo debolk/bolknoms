dfd70a69cecf6ee04639e775ed96d710e4aa5426
Jakob Buis <jpabuis@cubiso.nl>
2011-08-01 16:28:11 +0200

    doc fix

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -59,7 +59,7 @@
 
 <h2>Afmelden</h2>
 <p>
-    Na je aanmelding ontvang je een e-mail van bolknoms@bolk.nl ter bevestiging. Deze e-mail bevat een link om je uit te schrijven.
+    Na je aanmelding ontvang je een e-mail van bolknoms@debolk.nl ter bevestiging. Deze e-mail bevat een link om je uit te schrijven.
     Let op: als de inschrijving is gesloten, kun je je ook niet meer afmelden voor een maaltijd.
     Neem bij problemen contact op met het bestuur via
     <a href="mailto:bestuur@nieuwedelft.nl">bestuur@nieuwedelft.nl</a> of 015 212 60 12.
