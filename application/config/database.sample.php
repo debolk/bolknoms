60ac8aacbfbaef9f32dbb01909eed0465e1217ee
Jakob Buis <jpabuis@gmail.com>
2012-01-11 14:20:01 +0100

    Removed some unused code from the sample database configuration

--- application/config/database.sample.php
+++ application/config/database.sample.php
@@ -1,38 +1,20 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-$configuration = array
+return array
 (
-	'default' => array
-	(
-		'type'       => 'mysql',
-		'connection' => array(
-			'hostname'   => 'localhost',
-			 //'database'   => '', // Determined dynamically below
-			'username'   => 'bolknoms',
-			'password'   => 'xisAvurA',
-			'persistent' => true,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => true,
-		'profiling'    => true,
-	),
+    'default' => array
+    (
+        'type'       => 'mysql',
+        'connection' => array(
+            'hostname'   => 'localhost',
+            'database'   => 'bolknoms_development',
+            'username'   => 'bolknoms',
+            'password'   => 'xisAvurA',
+            'persistent' => true,
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'caching'      => true,
+        'profiling'    => true,
+    ),
 );
-
-// Determine database name
-$db = '';
-switch (Kohana::$environment)
-{
-    case Kohana::DEVELOPMENT:
-        $db = 'bolknoms_development'; break;
-    case Kohana::TESTING:
-        $db = 'bolknoms_testing'; break;
-    case Kohana::STAGING:
-        $db = 'bolknoms_staging'; break;
-    default:
-        $db = 'inschrijven';
-}
-$configuration['default']['connection']['database'] = $db;
-
-// Send configuration
-return $configuration;
