81097814e174622507b832da276b032e078caafc
Jakob Buis <jpabuis@gmail.com>
2012-01-11 14:43:18 +0100

    Removed email from website to prevent SPAM

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -61,8 +61,7 @@
 <p>
     Na je aanmelding ontvang je een e-mail van no-reply@debolk.nl ter bevestiging. Deze e-mail bevat een link om je uit te schrijven.
     Let op: als de inschrijving is gesloten, kun je je ook niet meer afmelden voor een maaltijd.
-    Neem bij problemen contact op met het bestuur via
-    <a href="mailto:bestuur@nieuwedelft.nl">bestuur@nieuwedelft.nl</a> of 015 212 60 12.
+    Neem bij problemen contact op met het bestuur via het bekende e-mailadres of 015 212 60 12.
 </p>
 
 <p class="navigation">
