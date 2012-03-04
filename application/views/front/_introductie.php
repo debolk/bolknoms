c719d12c8727328f7920ef23bed4757e5942c285
Jakob Buis <jpabuis@cubiso.nl>
2012-03-04 10:14:19 +0100

    BolkNoms is now called bolknoms

--- application/views/front/_introductie.php
+++ application/views/front/_introductie.php
@@ -1,4 +1,4 @@
-<h1>BolkNoms</h1>
+<h1>bolknoms</h1>
 <p class="introduction">
     Van 27&nbsp;februari&nbsp;2012 tot en met 19&nbsp;april&nbsp;2012 draaien we een proef voor het 
     verplicht inschrijven voor de eettafel. Met deze (extreem hippe) tool kun je je aanmelden voor een maaltijd op De Bolk.
