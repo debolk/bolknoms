a80755093304ee64727091d18a95b16112f6e491
Jakob Buis <jpabuis@cubiso.nl>
2012-03-04 10:22:25 +0100

    added the Bolknoms logo

--- application/views/front/_introductie.php
+++ application/views/front/_introductie.php
@@ -1,3 +1,4 @@
+<img src="images/logo.png" width="128" height="128" alt="Logo bolknoms" class="logo" />
 <h1>bolknoms</h1>
 <p class="introduction">
     Van 27&nbsp;februari&nbsp;2012 tot en met 19&nbsp;april&nbsp;2012 draaien we een proef voor het 
