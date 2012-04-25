8246fa9cbd3ea4fe8522fd5bfae2b82d1caee3fc
Jakob Buis <jpabuis@cubiso.nl>
2012-04-25 18:28:57 +0200

    updated introductory text to represent the fact that the introductory period has been completed

--- application/views/front/_introductie.php
+++ application/views/front/_introductie.php
@@ -1,6 +1,5 @@
 <img src="images/logo.png" width="128" height="128" alt="Logo bolknoms" class="logo" />
 <h1>bolknoms</h1>
 <p class="introduction">
-    Van 27&nbsp;februari&nbsp;2012 tot en met 19&nbsp;april&nbsp;2012 draaien we een proef voor het 
-    verplicht inschrijven voor de eettafel. Met deze (extreem hippe) tool kun je je aanmelden voor een maaltijd op De Bolk.
+	Welkom op bolknoms. Hier kun je je snel en betrouwbaar inschrijven voor de maaltijden op De Bolk.
 </p>
