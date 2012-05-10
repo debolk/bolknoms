84524a5071a786974c68451ea8fc99a317a5f885
Jakob Buis <jpabuis@cubiso.nl>
2012-05-10 16:47:08 +0200

    added notification that there's no next meal planned

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -7,33 +7,39 @@
     <?php echo Helper_Form::error_messages_for($validation); ?>
 <?php endif; ?>
 
-<form action="/aanmelden" method="post" accept-charset="utf-8" class="clearfix">
-    <p>
-        <label for="date" class="label">Volgende eettafel</label>
-        <?php echo $upcoming_meal; ?>
-    </p>
-    <?php if ($upcoming_meal->open_for_registrations()): ?>
+<?php if ($upcoming_meal === null): ?>
+    <form action="/aanmelden" method="post" accept-charset="utf-8" class="clearfix">
         <p>
-            <label for="name" class="label">Naam</label>
-            <input type="text" name="name" value="" />
-            <small>Gebruik je volledige voor- en achternaam. Onduidelijke inschrijvingen worden vernietigd.</small>
+            <label for="date" class="label">Volgende eettafel</label>
+            <?php echo $upcoming_meal; ?>
         </p>
-        <p>
-            <input type="submit" value="Aanmelden"/>
-        </p>    
-    <?php else: ?>
-        <p class="notification warning">
-            Sorry, de deadline is verstreken. Je kunt je niet meer aanmelden voor de maaltijd
-            van vandaag. Je kunt je nog wel <a href="/uitgebreid-inschrijven">aanmelden voor een andere eettafel</a>.
-        </p>
-    <?php endif; ?>
-</form>
+        <?php if ($upcoming_meal->open_for_registrations()): ?>
+            <p>
+                <label for="name" class="label">Naam</label>
+                <input type="text" name="name" value="" />
+                <small>Gebruik je volledige voor- en achternaam. Onduidelijke inschrijvingen worden vernietigd.</small>
+            </p>
+            <p>
+                <input type="submit" value="Aanmelden"/>
+            </p>    
+        <?php else: ?>
+            <p class="notification warning">
+                Sorry, de deadline is verstreken. Je kunt je niet meer aanmelden voor de maaltijd
+                van vandaag. Je kunt je nog wel <a href="/uitgebreid-inschrijven">aanmelden voor een andere eettafel</a>.
+            </p>
 
-<p>
-    Wil je je aanmelden voor meerdere dagen tegelijkertijd, je vrienden meenemen, of 
-    heb je speciale eisen m.b.t. voedsel? Schrijf je dan in via
-    <a href="/uitgebreid-inschrijven">uitgebreid aanmelden</a>.
-</p>
+            <p>
+                Wil je je aanmelden voor meerdere dagen tegelijkertijd, je vrienden meenemen, of 
+                heb je speciale eisen m.b.t. voedsel? Schrijf je dan in via
+                <a href="/uitgebreid-inschrijven">uitgebreid aanmelden</a>.
+            </p>
+        <?php endif; ?>
+    </form>
+<?php else: ?>
+    <p class="notification warning">
+        Er is geen volgende eettafel ingepland.
+    </p>
+<?php endif; ?>
 
 <?php echo View::factory('front/_spelregels'); ?>
 
