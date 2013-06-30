230ee0c644bc437ba71eaeeae9819284cd929146
Jakob Buis <jpabuis@gmail.com>
2013-07-01 00:15:20 +0200

    updated bootstrap.php to match Kohana 3.3; routes do not work

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -3,48 +3,56 @@
 // -- Environment setup --------------------------------------------------------
 
 // Load the core Kohana class
-require SYSPATH.'classes/kohana/core'.EXT;
+require SYSPATH.'classes/Kohana/Core'.EXT;
 
-if (is_file(APPPATH.'classes/kohana'.EXT))
+if (is_file(APPPATH.'classes/Kohana'.EXT))
 {
-	// Application extends the core
-	require APPPATH.'classes/kohana'.EXT;
+  // Application extends the core
+  require APPPATH.'classes/Kohana'.EXT;
 }
 else
 {
-	// Load empty core extension
-	require SYSPATH.'classes/kohana'.EXT;
+  // Load empty core extension
+  require SYSPATH.'classes/Kohana'.EXT;
 }
 
 /**
  * Set the default time zone.
  *
- * @see  http://kohanaframework.org/guide/using.configuration
- * @see  http://php.net/timezones
+ * @link http://kohanaframework.org/guide/using.configuration
+ * @link http://www.php.net/manual/timezones
  */
 date_default_timezone_set('Europe/Amsterdam');
 
 /**
  * Set the default locale.
  *
- * @see  http://kohanaframework.org/guide/using.configuration
- * @see  http://php.net/setlocale
+ * @link http://kohanaframework.org/guide/using.configuration
+ * @link http://www.php.net/manual/function.setlocale
  */
-setlocale (LC_ALL, 'nl_NL','nl', 'du', 'dutch');
+setlocale(LC_ALL, 'nl_NL', 'nl', 'du', 'dutch');
 
 /**
  * Enable the Kohana auto-loader.
  *
- * @see  http://kohanaframework.org/guide/using.autoloading
- * @see  http://php.net/spl_autoload_register
+ * @link http://kohanaframework.org/guide/using.autoloading
+ * @link http://www.php.net/manual/function.spl-autoload-register
  */
 spl_autoload_register(array('Kohana', 'auto_load'));
 
 /**
+ * Optionally, you can enable a compatibility auto-loader for use with
+ * older modules that have not been updated for PSR-0.
+ *
+ * It is recommended to not enable this unless absolutely necessary.
+ */
+//spl_autoload_register(array('Kohana', 'auto_load_lowercase'));
+
+/**
  * Enable the Kohana auto-loader for unserialization.
  *
- * @see  http://php.net/spl_autoload_call
- * @see  http://php.net/manual/var.configuration.php#unserialize-callback-func
+ * @link http://www.php.net/manual/function.spl-autoload-call
+ * @link http://www.php.net/manual/var.configuration#unserialize-callback-func
  */
 ini_set('unserialize_callback_func', 'spl_autoload_call');
 
@@ -63,7 +71,7 @@ I18n::lang('nl');
  */
 if (isset($_SERVER['KOHANA_ENV']))
 {
-	Kohana::$environment = constant('Kohana::'.strtoupper($_SERVER['KOHANA_ENV']));
+  Kohana::$environment = constant('Kohana::'.strtoupper($_SERVER['KOHANA_ENV']));
 }
 
 /**
@@ -75,19 +83,21 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - string   index_file  name of your index file, usually "index.php"       index.php
  * - string   charset     internal character set used for input and output   utf-8
  * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - integer  cache_life  lifetime, in seconds, of items cached              60
  * - boolean  errors      enable or disable error handling                   TRUE
  * - boolean  profile     enable or disable internal profiling               TRUE
  * - boolean  caching     enable or disable internal caching                 FALSE
+ * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-	'base_url'   => '/',
-    'index_file' => '',
-    'caching'    => (Kohana::$environment === Kohana::PRODUCTION),
-    'profile'    => (Kohana::$environment !== Kohana::PRODUCTION),
-	'errors'	 => true
+  'base_url'   => '/',
+  'index_file' => '',
+  'caching'    => (Kohana::$environment === Kohana::PRODUCTION),
+  'profile'    => (Kohana::$environment !== Kohana::PRODUCTION),
+  'errors'   => true,
 ));
 if (Kohana::$environment == Kohana::DEVELOPMENT) {
-	ini_set('display_errors', 'on');
+  ini_set('display_errors', 'on');
 }
 
 /**
@@ -104,73 +114,78 @@ Kohana::$config->attach(new Config_File);
  * Enable modules. Modules are referenced by a relative or absolute path.
  */
 $modules = array(
-    'database'   => MODPATH.'database',   // Database access
-	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	'flash'      => MODPATH.'flash',      // Flash messages
+  'database'   => MODPATH.'database',   // Database access
+  'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+  'flash'      => MODPATH.'flash',      // Flash messages
 );
 // Enable unittesting, except on production
 if (Kohana::$environment !== Kohana::PRODUCTION) {
-    $modules['unittest'] = MODPATH.'unittest';
+  $modules['unittest'] = MODPATH.'unittest';
 }
 Kohana::modules($modules);
 
-/**
- * Set the routes. Each route must have a minimum of a name, a URI and a set of
- * defaults for the URI.
+/*
+ * Set a cookie salt to secure cookies
  */
-Route::set('inschrijven', 'inschrijven')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'index',
-	));
-Route::set('inschrijven_specifiek', 'inschrijven/<id>', array('id' => '\d+'))
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'inschrijven_specifiek',
-	));
-Route::set('aanmelden_specifiek', 'aanmelden/<id>', array('id' => '\d+'))
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'aanmelden_specifiek',
-	));
-Route::set('uitgebreid-inschrijven', 'uitgebreid-inschrijven')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'uitgebreidinschrijven',
-	));
-Route::set('uitgebreidaanmelden', 'uitgebreidaanmelden')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'uitgebreidaanmelden',
-	));
-Route::set('aanmelden', 'aanmelden')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'aanmelden',
-	));
-Route::set('afmelden', 'afmelden/<id>/<salt>', array('id' => '\d+', 'salt' => '\w+'))
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'afmelden',
-	));
-Route::set('disclaimer', 'disclaimer')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'disclaimer',
-	));
-Route::set('privacy', 'privacy')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'privacy',
-	));
+Cookie::$salt = getenv('COOKIE_SALT');
 
-Route::set('default', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'front',
-		'action'     => 'index',
-	));
+ /**
+  * Set the routes. Each route must have a minimum of a name, a URI and a set of
+  * defaults for the URI.
+  */
+Route::set('inschrijven', 'inschrijven')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'index',
+ ));
+Route::set('inschrijven_specifiek', 'inschrijven/<id>', array('id' => '\d+'))
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'inschrijven_specifiek',
+ ));
+Route::set('aanmelden_specifiek', 'aanmelden/<id>', array('id' => '\d+'))
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'aanmelden_specifiek',
+ ));
+Route::set('uitgebreid-inschrijven', 'uitgebreid-inschrijven')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'uitgebreidinschrijven',
+ ));
+Route::set('uitgebreidaanmelden', 'uitgebreidaanmelden')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'uitgebreidaanmelden',
+ ));
+Route::set('aanmelden', 'aanmelden')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'aanmelden',
+ ));
+Route::set('afmelden', 'afmelden/<id>/<salt>', array('id' => '\d+', 'salt' => '\w+'))
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'afmelden',
+ ));
+Route::set('disclaimer', 'disclaimer')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'disclaimer',
+ ));
+Route::set('privacy', 'privacy')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'privacy',
+ ));
+
+ Route::set('default', '(<controller>(/<action>(/<id>)))')
+ ->defaults(array(
+   'controller' => 'front',
+   'action'     => 'index',
+ ));
 
 Route::set('error', 'error/<action>', array('action' => '[0-9]++'))
 ->defaults(array(
-	'controller' => 'error'
+ 'controller' => 'error'
 ));
